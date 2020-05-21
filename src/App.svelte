6b9371501bc7eabcdd6168f0dfb55eae6180b393
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-21 14:17:38 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+
+</script>
+
+<style>
+  form {
+    --text-color: #afafaf;
+    max-width: 500px;
+  }
+
+  .field {
+    width: 100%;
+    position: relative;
+    border-bottom: 2px dashed var(--text-color);
+    margin: 4rem auto 1rem;
+  }
+
+  .label {
+    color: var(--text-color);
+    font-size: 1.2rem;
+  }
+
+  .field::after {
+    content: "";
+    position: relative;
+    display: block;
+    height: 4px;
+	width: 100%;
+	backgro
+  }
+
+  .input {
+    outline: none;
+    border: none;
+    overflow: hidden;
+    margin: 0;
+    padding: 0.25rem 0;
+    background: none;
+    color: white;
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  .input:valid {
+    color: yellowgreen;
+  }
+  .input:invalid {
+    color: orangered;
+  }
+</style>
+
+<main>
+  <form>
+    <div class="field">
+      <input type="email" name="email" placeholder="" class="input" />
+      <label for="email" class="label">Email</label>
+    </div>
+
+    <div class="field">
+      <input type="password" name="password" placeholder="" class="input" />
+      <label for="password" class="label">Password</label>
+    </div>
+
+  </form>
+</main>
